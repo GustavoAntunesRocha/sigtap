608b94d6e60d081a9be4fd634a3ddd6dbf572cc5
Gustavo <gustavo.antunes.rocha@outlook.com>
2021-09-17 15:13:28 -0300

    Corrigido carregamento dos arquivos e retirado função de remover acentos

--- Projetos/sigtapCarregamentoDados/src/main/java/br/com/gustavo/sigtapCarregamentoDados/CarregaBD.java
+++ Projetos/sigtapCarregamentoDados/src/main/java/br/com/gustavo/sigtapCarregamentoDados/CarregaBD.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.Normalizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,17 +33,16 @@ public class CarregaBD {
 	@Autowired
 	private ProcedimentoRepository procedimentoRepository;
 	
+	
 	public void carregaGrupos(String path) {
 		BufferedReader br = null;
 		String linha = "";
 		try {
 
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "ISO-8859-1"));
-			linha = br.readLine();
 			while ((linha = br.readLine()) != null) {
 
-				linha = removeSinais(linha);
-				Grupo grupo = new Grupo(Integer.parseInt(linha.substring(0, 1)), linha.substring(2, 101), linha.substring(102));
+				Grupo grupo = new Grupo(Integer.parseInt(linha.substring(0, 2)), linha.substring(2, 102), linha.substring(102));
 				grupoRepository.save(grupo);
 
 			}
@@ -70,12 +68,10 @@ public class CarregaBD {
 		try {
 
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "ISO-8859-1"));
-			linha = br.readLine();
 			while ((linha = br.readLine()) != null) {
 
-				linha = removeSinais(linha);
-				Grupo grupo = grupoRepository.findByCodigo(Integer.parseInt(linha.substring(0, 1)));
-				SubGrupo subGrupo = new SubGrupo(Integer.parseInt(linha.substring(2, 3)), grupo, linha.substring(4, 103), linha.substring(104));
+				Grupo grupo = grupoRepository.findByCodigo(Integer.parseInt(linha.substring(0, 2)));
+				SubGrupo subGrupo = new SubGrupo(Integer.parseInt(linha.substring(2, 4)), grupo, linha.substring(4, 104), linha.substring(104));
 				subGrupoRepository.save(subGrupo);
 
 			}
@@ -101,13 +97,11 @@ public class CarregaBD {
 		try {
 
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "ISO-8859-1"));
-			linha = br.readLine();
 			while ((linha = br.readLine()) != null) {
 
-				linha = removeSinais(linha);
-				Grupo grupo = grupoRepository.findByCodigo(Integer.parseInt(linha.substring(0, 1)));
-				SubGrupo subGrupo = subGrupoRepository.findByCodigo(Integer.parseInt(linha.substring(2, 3)));
-				FormaOrganizacao formaOrganizacao = new FormaOrganizacao(Integer.parseInt(linha.substring(4, 5)), grupo, subGrupo, linha.substring(6, 105), linha.substring(106));
+				Grupo grupo = grupoRepository.findByCodigo(Integer.parseInt(linha.substring(0, 2)));
+				SubGrupo subGrupo = subGrupoRepository.findByCodigo(Integer.parseInt(linha.substring(2, 4)));
+				FormaOrganizacao formaOrganizacao = new FormaOrganizacao(Integer.parseInt(linha.substring(4, 6)), grupo, subGrupo, linha.substring(6, 106), linha.substring(106));
 				formaOrganizacaoRepository.save(formaOrganizacao);
 
 			}
@@ -133,30 +127,28 @@ public class CarregaBD {
 		try {
 
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "ISO-8859-1"));
-			linha = br.readLine();
 			while ((linha = br.readLine()) != null) {
 
-				linha = removeSinais(linha);
-				Grupo grupo = grupoRepository.findByCodigo(Integer.parseInt(linha.substring(0, 1)));
-				SubGrupo subGrupo = subGrupoRepository.findByCodigo(Integer.parseInt(linha.substring(2, 3)));
-				FormaOrganizacao formaOrganizacao = formaOrganizacaoRepository.findByCodigo(Integer.parseInt(linha.substring(4, 5)));
+				Grupo grupo = grupoRepository.findByCodigo(Integer.parseInt(linha.substring(0, 2)));
+				SubGrupo subGrupo = subGrupoRepository.findByCodigo(Integer.parseInt(linha.substring(2, 4)));
+				FormaOrganizacao formaOrganizacao = formaOrganizacaoRepository.findByCodigo(Integer.parseInt(linha.substring(4, 6)));
 				
 				Procedimento procedimento = new Procedimento();
-				procedimento.setCodigo(Integer.parseInt(linha.substring(0, 9)));
+				procedimento.setCodigo(Integer.parseInt(linha.substring(0, 10)));
 				procedimento.setGrupo(grupo);
 				procedimento.setSubGrupo(subGrupo);
 				procedimento.setFormaOrganizacao(formaOrganizacao);
-				procedimento.setNome(linha.substring(10, 259));
-				procedimento.setComplexidade(linha.substring(0, 0));
-				procedimento.setSexo(linha.substring(261, 261));
-				procedimento.setQtdMaxima(Integer.parseInt(linha.substring(262, 265)));
-				procedimento.setQtdDiasPermanencia(Integer.parseInt(linha.substring(266, 269)));
-				procedimento.setQtdPontos(Integer.parseInt(linha.substring(270, 273)));
-				procedimento.setIdadeMinima(Integer.parseInt(linha.substring(274, 277)));
-				procedimento.setIdadeMaxima(Integer.parseInt(linha.substring(278, 281)));
-				procedimento.setValorServicoHospitalar(Integer.parseInt(linha.substring(282, 291)));
-				procedimento.setValorServicoAmbulatorial(Integer.parseInt(linha.substring(292, 301)));
-				procedimento.setValorServicoProfissional(Integer.parseInt(linha.substring(302, 311)));
+				procedimento.setNome(linha.substring(10, 260));
+				procedimento.setComplexidade(linha.substring(260, 261));
+				procedimento.setSexo(linha.substring(261, 262));
+				procedimento.setQtdMaxima(Integer.parseInt(linha.substring(262, 266)));
+				procedimento.setQtdDiasPermanencia(Integer.parseInt(linha.substring(266, 270)));
+				procedimento.setQtdPontos(Integer.parseInt(linha.substring(270, 274)));
+				procedimento.setIdadeMinima(Integer.parseInt(linha.substring(274, 278)));
+				procedimento.setIdadeMaxima(Integer.parseInt(linha.substring(278, 282)));
+				procedimento.setValorServicoHospitalar(Integer.parseInt(linha.substring(282, 292)));
+				procedimento.setValorServicoAmbulatorial(Integer.parseInt(linha.substring(292, 302)));
+				procedimento.setValorServicoProfissional(Integer.parseInt(linha.substring(302, 312)));
 				procedimento.setDataCompetencia(linha.substring(324));
 				procedimentoRepository.save(procedimento);
 
@@ -175,12 +167,5 @@ public class CarregaBD {
 				}
 			}
 		}
-	}
-	
-	private static String removeSinais(String entrada) {
-		String sa = Normalizer.normalize(entrada, Normalizer.Form.NFD);
-
-		sa = sa.replaceAll("\"", "");
-		return sa.replaceAll("\\p{M}", "");
 	}
 }
--- Projetos/sigtapCarregamentoDados/src/main/java/br/com/gustavo/sigtapCarregamentoDados/SigtapCarregamentoDadosApplication.java
+++ Projetos/sigtapCarregamentoDados/src/main/java/br/com/gustavo/sigtapCarregamentoDados/SigtapCarregamentoDadosApplication.java
@@ -1,13 +1,34 @@
 package br.com.gustavo.sigtapCarregamentoDados;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class SigtapCarregamentoDadosApplication {
 
+	public static final String BASE_PATH = "C:\\Users\\gustavo.antunes\\Desktop\\TabelaUnificada_202108_v2108021412";
+	public static final String IN_DIR_GRUPOS = BASE_PATH + "\\tb_grupo.txt";
+	public static final String IN_DIR_SUBGRUPOS = BASE_PATH + "\\tb_sub_grupo.txt";
+	public static final String IN_DIR_FORMAORGANIZACAO = BASE_PATH + "\\tb_forma_organizacao.txt";
+	public static final String IN_DIR_PROCEDIMENTOS = BASE_PATH + "\\tb_procedimento.txt";
+	
+	@Autowired
+	private CarregaBD carregaBD;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SigtapCarregamentoDadosApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	private void processa() {
+		carregaBD.carregaGrupos(IN_DIR_GRUPOS);
+		carregaBD.carregaSubGrupos(IN_DIR_SUBGRUPOS);
+		carregaBD.carregaFormaOrganizacao(IN_DIR_FORMAORGANIZACAO);
+		carregaBD.carregaProcedimentos(IN_DIR_PROCEDIMENTOS);
+	}
 }
