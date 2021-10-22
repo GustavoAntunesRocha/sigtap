0cedfbdf4b3d464c79cb6f9f9457f0e672306af0
Gustavo <gustavo.antunes.rocha@outlook.com>
2021-10-22 10:35:34 -0300

    Criação do projeto da API

---
+++ Projetos/sigtapAPI/src/main/java/br/com/gustavo/sigtapAPI/SigtapApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.sigtapAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigtapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigtapApiApplication.class, args);
+	}
+
+}
---
+++ Projetos/sigtapAPI/src/test/java/br/com/gustavo/sigtapAPI/SigtapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.sigtapAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigtapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
